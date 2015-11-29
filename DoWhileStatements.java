278d63375ba9c0498361b2b59e634781e2fef08a
mtang5 <mgtang28@gmail.com>
2015-11-28 23:31:27 -0800

    Do While loop program

---
+++ DoWhileStatements.java
@@ -0,0 +1,25 @@
+package lesson;
+
+public class DoWhileStatements 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		int a = 0;
+		
+		while(a <= -1)
+		{
+			System.out.println(a); // print out nothing
+			a++;
+		}
+		
+		int b = 0;
+		
+		do {
+			//System.out.println(b);
+			b++;
+		} while(b <= 10); // print first letter out if meet do-while requirements
+	}
+
+}
